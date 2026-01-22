3bd6c512c5d2402ec962468c82ecfcf4b1c859a7
melodicht <marvincht@gmail.com>
2026-01-21 22:50:58 -0500

    Change fixed size arenas to use platform allocator

--- src/platform/main.cpp
+++ src/platform/main.cpp
@@ -521,18 +521,7 @@ int main(int argc, char** argv)
 
     SDLGameCode gameCode = SDLLoadGameCode();
     GameMemory gameMemory = {};
-    gameMemory.permanentStorageSize = Megabytes(512 + 256);
-    gameMemory.permanentStorage = SDL_malloc(static_cast<size_t>(gameMemory.permanentStorageSize));
 #if SKL_INTERNAL
-    gameMemory.debugStorageSize = Megabytes(256);
-    gameMemory.debugStorage = SDL_malloc(static_cast<size_t>(gameMemory.debugStorageSize));
-
-    if (!gameMemory.debugStorage)
-    {
-        printf("SDL_malloc failed! SDL_Error: %s\n", SDL_GetError());
-        Assert(false);
-    }
-
     gameMemory.debugState = globalDebugState;
 #endif
     gameMemory.imGuiContext = imGuiContext;
