9b2209230439c27a246ddefefef4b0839ee7b382
Jianwen Ding <jianwen_ding@icloud.com>
2026-02-08 00:07:35 -0500

    Encapsulation cleanup

--- include/platform/platform_loop.h
+++ include/platform/platform_loop.h
@@ -13,7 +13,7 @@ struct SDLState;
 struct GameInput;
 struct SDLMemoryBlock;
 
-// Represent 
+// Represents the state of a memory block durring looping
 struct LoopMemoryFlags {
 private:
     enum SDLMemoryFlags
@@ -28,7 +28,8 @@ public:
     friend class LoopUtils;
 };
 
-// Represents 
+// Represents the state of looping and stored input information
+// For looping.
 struct LoopState {
 private: 
     enum class LoopedLiveEditingState : u8
@@ -57,37 +58,36 @@ public:
 // ensuring tight encapsulation.
 class LoopUtils {
 private:
-    file_global const char* SDLGetInputFilePath();
-    file_global b8 SDLIsInLoop(const SDLState* state);
-    file_global void SDLClearBlocksByMask(SDLState* state, LoopMemoryFlags::SDLMemoryFlags mask);
-    file_global void SDLBeginInputPlayback(SDLState* state);
-    file_global void SDLEndInputPlayback(SDLState* state);
-    file_global void SDLBeginRecordingInput(SDLState* state);
-    file_global void SDLEndRecordingInput(SDLState* state);
-    file_global void SDLRecordStdString(SDLState* state, const std::string* str);
-    file_global void SDLRecordStdSetOfString(SDLState* state, const std::set<std::string>* strings);
-    file_global void SDLRecordInput(SDLState* state, const GameInput* gameInput);
-    file_global void SDLPlaybackStdString(SDLState* state, std::set<std::string>* strings);
-    file_global void SDLPlaybackStdSetOfString(SDLState* state, std::set<std::string>* strings);
+    static const char* SDLGetInputFilePath();
+    static b8 SDLIsInLoop(const SDLState* state);
+    static void SDLClearBlocksByMask(SDLState* state, LoopMemoryFlags::SDLMemoryFlags mask);
+    static void SDLBeginInputPlayback(SDLState* state);
+    static void SDLEndInputPlayback(SDLState* state);
+    static void SDLBeginRecordingInput(SDLState* state);
+    static void SDLEndRecordingInput(SDLState* state);
+    static void SDLRecordStdString(SDLState* state, const std::string* str);
+    static void SDLRecordStdSetOfString(SDLState* state, const std::set<std::string>* strings);
+    static void SDLRecordInput(SDLState* state, const GameInput* gameInput);
+    static void SDLPlaybackStdString(SDLState* state, std::set<std::string>* strings);
+    static void SDLPlaybackStdSetOfString(SDLState* state, std::set<std::string>* strings);
 
     // Returns if a reset happen?
-    file_global b8 SDLPlaybackInput(SDLState* state, GameInput* gameInput);
+    static b8 SDLPlaybackInput(SDLState* state, GameInput* gameInput);
 
 public:
-    // Checks whether SDL is 
-    file_global b8 GetIsStateInLoop(const SDLState* state);
+    static b8 GetIsStateInLoop(const SDLState* state);
 
-    file_global void ToggleLoopedLiveEditingState(SDLState* state);
+    static void ToggleLoopedLiveEditingState(SDLState* state);
 
     // Based on live editing state, stores or plays game input
     // Produces a boolean on whether to reload game code.
     // TODO(marvin): Producing a b8ean for ProcessInputWithLooping feels very contrived. Keep eyes opened for a better way.
-    file_global b8 ProcessInputWithLooping(SDLState* state, GameInput* gameInput);
+    static b8 ProcessInputWithLooping(SDLState* state, GameInput* gameInput);
 
     // Gets and sets allocation flags
-    file_global void SetBlockFlagLoopAllocated(SDLMemoryBlock* flag);
-    file_global void SetBlockFlagLoopFreed(SDLMemoryBlock* flag);
-    file_global void SetBlockFlagLoopNone(SDLMemoryBlock* flag);
+    static void SetBlockFlagLoopAllocated(SDLMemoryBlock* flag);
+    static void SetBlockFlagLoopFreed(SDLMemoryBlock* flag);
+    static void SetBlockFlagLoopNone(SDLMemoryBlock* flag);
 
-    file_global b8 GetBlockFlagLoopAllocated(const SDLMemoryBlock* flag);
+    static b8 GetBlockFlagLoopAllocated(const SDLMemoryBlock* flag);
 };
